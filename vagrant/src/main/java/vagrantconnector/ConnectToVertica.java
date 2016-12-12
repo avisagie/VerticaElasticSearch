5efd603114ecda9f9ad1e15e706a11bca5516b68
Onkabetse <stlonk001@myuct.ac.za>
2016-12-12 14:27:38 +0200

    adding tweets to the database

---
+++ vagrant/src/main/java/vagrantconnector/ConnectToVertica.java
@@ -0,0 +1,115 @@
+package vagrantconnector;
+
+import java.sql.SQLTransientConnectionException;
+import java.sql.Statement;
+import java.sql.SQLInvalidAuthorizationSpecException;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import com.vertica.jdbc.VerticaConnection;
+import com.vertica.jdbc.VerticaCopyStream;
+
+
+
+public class ConnectToVertica {
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		
+		Properties properties = new Properties();
+		properties.put("user", "dbadmin");
+		properties.put("password", "mydatA6asepa55w0rd");
+		properties.put("loginTimeout", "35");
+		properties.put("binaryBatchInsert", "true");
+		properties.put("AutoCommit", "false");
+		
+		Connection conn;
+		try {
+			
+			conn = DriverManager.getConnection("jdbc:vertica://localhost:5433/mydatabase", properties);
+			System.out.println("Connected to the database");
+			Statement statement = conn.createStatement();
+			
+			statement.execute("DROP TABLE IF EXISTS mytable");
+			
+			/**
+			 *  stmt.execute("CREATE TABLE customers (Last_Name char(50), "
+                            + "First_Name char(50),Email char(50), "
+                            + "Phone_Number char(15))");
+			 */
+			statement.execute("CREATE TABLE mytable ("
+					+ "id VARBINARY(200) PRIMARY KEY,"
+					+ "created_at VARCHAR(100),"
+					+ "favourite_count INT,"
+					+ "access_level INT,"
+					+ "in_reply_to_status_id VARBINARY(200),"
+					+ "in_reply_to_user_id VARBINARY(200),"
+					+ "language VARCHAR(10),"
+					+ "source VARCHAR(200),"
+					+ "is_favourited BOOLEAN,"
+					+ "is_possibly_sensitive BOOLEAN,"
+					+ "is_retweet BOOLEAN,"
+					+ "is_retweeted_by_me BOOLEAN,"
+					+ "is_truncated BOOLEAN )");
+			
+			String copyQuery = "COPY mytable FROM STDIN " + "DELIMITER '|' DIRECT ENFORCELENGTH";
+			
+			VerticaCopyStream stream = new VerticaCopyStream((VerticaConnection) conn, copyQuery);
+			
+			
+			stream.start();
+			
+			String filename = "C:\\Users\\User\\Desktop\\Vastech_Internship\\VerticaElasticSearch\\doc\\tweets.txt";
+			
+			File inputFile = new File(filename);
+			
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			
+			stream.addStream(inputStream);
+			
+			stream.execute();
+			
+			List<Long> rejects = stream.getRejects();
+			
+			int numRejects = rejects.size();
+			
+		
+			
+			System.out.println("Number of rows rejected is load " + numRejects);
+			
+			Iterator<Long> rejit = rejects.iterator();
+		
+			while(rejit.hasNext()){
+				System.out.println("Rejected row " + rejit.next());
+			}
+			
+			long results = stream.finish();
+			System.out.println("success-results " + results);
+			
+			conn.commit();
+		
+			
+			conn.close();
+		} catch(SQLTransientConnectionException connException) {
+			System.out.print("Network connection issue: " + connException.getMessage());
+			System.out.println();
+			
+			return;
+		} catch(SQLInvalidAuthorizationSpecException authException) {
+			System.out.print("Could not log into database: " + authException.getMessage());
+			
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+}
